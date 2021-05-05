d07d971bdc15de76cddc720d4509a56207583950
norahsuw <80370224+norahsuw@users.noreply.github.com>
2021-05-05 17:34:00 +0800

    Update combine.py

--- combine.py
+++ combine.py
@@ -1,11 +1,8 @@
 import bar_chart_race as bcr
-import pandas as pd
-df = bcr.load_dataset('chart')
-
-
+df = bcr.load_dataset('covid19_tutorial')
 bcr.bar_chart_race(
     df=df,
-    filename='chart.mp4',
+    filename='covid19_horiz.mp4',
     orientation='h',
     sort='desc',
     n_bars=6,
@@ -21,7 +18,7 @@ bcr.bar_chart_race(
                                       's': f'Total deaths: {v.nlargest(6).sum():,.0f}',
                                       'ha': 'right', 'size': 8, 'family': 'Courier New'},
     perpendicular_bar_func='median',
-    period_length=800,
+    period_length=500,
     figsize=(5, 3),
     dpi=144,
     cmap='dark12',
