03b53a1665b8e04faa8020fdde99570e17a9190d
norahsuw <80370224+norahsuw@users.noreply.github.com>
2021-05-07 17:25:57 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import bar_chart_race as bcr
 import pandas as pd
-df = pd.read_csv('Hualien2.csv', index_col='Month', parse_dates=['Month'])
+df = pd.read_csv('Hualien.csv',index_col="Month",parse_dates=['Month'])
 print(df.tail())
 
 bcr.bar_chart_race(
 df=df,
-filename='Hualien.mp4',
+filename='Hualien number of visitors.mp4',
 orientation='h',
 sort='desc',
-n_bars=6,
+n_bars=10,
 fixed_order=False,
 fixed_max=True,
 steps_per_period=10,
@@ -18,14 +18,14 @@ bar_size=.95,
 period_label={'x': .99, 'y': .25, 'ha': 'right', 'va': 'center'},
 period_fmt='%B %d, %Y',
 period_summary_func=lambda v, r: {'x': .99, 'y': .18,
-                                  's': f'Total population: {v.nlargest(6).sum():,.of}',
+                                  's': f'Total: {v.nlargest(6).sum():,.0f}',
                                   'ha': 'right', 'size': 8, 'family': 'Courier New'},
 perpendicular_bar_func='median',
 period_length=800,
 figsize=(5,3),
 dpi=144,
 cmap='dark12',
-title='Hualien',
+title='Hualien number of visitors',
 title_size='',
 bar_label_size=7,
 tick_label_size=7,
